55290bd6506071defa5dfbc7f8d3b84c950f483e
Nuriy_ff06 <Nuriy_ff06@STHQ0118-06>
2023-06-21 09:07:43 +0400

    Add project files.

---
+++ WpfApp3/Repository/Repo.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp3.Models;
+
+namespace WpfApp3.Repository
+{
+    public class Repo
+    {
+        ObservableCollection<Author> _authors = new ObservableCollection<Author>();
+        SqlConnection conn;
+        string cs = ConfigurationManager.ConnectionStrings["myConn"].ConnectionString;
+
+        public Repo()
+        {
+            using (conn = new SqlConnection())
+            {
+                conn.ConnectionString = cs;
+                conn.Open();
+
+                SqlDataReader reader = null;
+
+                string query = "SELECT * FROM Authors";
+                using (var command = new SqlCommand(query, conn))
+                {
+                    reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Author author = new Author
+                        {
+                            Id = int.Parse(reader[0].ToString()),
+                            FirstName = reader[1].ToString(),
+                            LastName = reader[2].ToString()
+                        };
+                        _authors.Add(author);
+                    }
+                }
+            }
+        }
+
+        public ObservableCollection<Author> GetAll()
+        {
+            return _authors;
+        }
+
+        public void Insert(int id,string firstName,string lastName)
+        {
+            using (var conn = new SqlConnection())
+            {
+                conn.ConnectionString = cs;
+                conn.Open();
+
+
+                string query = $@" INSERT INTO Authors(Id,FirstName,LastName)
+                                VALUES(@id,@firstName,@lastName)";
+
+                var paramId = new SqlParameter();
+                paramId.ParameterName = "@id";
+                paramId.SqlDbType = SqlDbType.Int;
+                paramId.Value = id;
+
+                var paramName = new SqlParameter();
+                paramName.ParameterName = "@firstName";
+                paramName.SqlDbType = SqlDbType.NVarChar;
+                paramName.Value = firstName;
+
+                var paramSurname = new SqlParameter();
+                paramSurname.ParameterName = "@lastName";
+                paramSurname.SqlDbType = SqlDbType.NVarChar;
+                paramSurname.Value = lastName;
+
+                using (var command = new SqlCommand(query, conn))
+                {
+                    command.Parameters.Add(paramId);
+                    command.Parameters.Add(paramName);
+                    command.Parameters.Add(paramSurname);
+
+                    var result = command.ExecuteNonQuery();
+                }
+                Author author = new Author
+                {
+                    Id = id,
+                    FirstName = firstName,
+                    LastName = lastName
+                };
+                _authors.Add(author);
+            }
+        }
+    }
+}
---
+++ WpfApp3/ViewModels/MainWindowViewModels.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using WpfApp3.Commands;
+using WpfApp3.Models;
+using WpfApp3.Repository;
+
+namespace WpfApp3.ViewModels
+{
+    public class MainWindowViewModels : BaseViewModel
+    {
+
+        private ObservableCollection<Author> allAuthors;
+
+        public ObservableCollection<Author> AllAuthors
+        {
+            get { return allAuthors; }
+            set { allAuthors = value; }
+        }
+
+
+
+        public Repo AuthorsRepo { get; set; }
+        public RelayCommand InsertCommand { get; set; }
+        public RelayCommand DeleteCommand { get; set; }
+        public RelayCommand UpdateCommand { get; set; }
+        public RelayCommand ShowAllCommand { get; set; }
+
+        private int id;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; OnPropertyChanged(); }
+        }
+
+        private string firstName;
+
+        public string Firstname
+        {
+            get { return firstName; }
+            set { firstName = value; OnPropertyChanged(); }
+        }
+
+        private string lastName;
+
+        public string Lastname
+        {
+            get { return lastName; }
+            set { lastName = value; OnPropertyChanged(); }
+        }
+
+       
+
+
+        public MainWindowViewModels()
+        {
+            AuthorsRepo = new Repo();
+            AllAuthors =AuthorsRepo.GetAll();
+
+            InsertCommand = new RelayCommand((obj) =>
+            {
+                AuthorsRepo.Insert(Id, Firstname, Lastname);
+            });
+
+            //ShowAllCommand = new RelayCommand((obj) =>
+            //{
+            //    AllAuthors = AuthorsRepo.GetAll();
+            //});
+
+
+           
+
+        }
+    }
+}
